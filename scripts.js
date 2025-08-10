9a62240de564738d142a5065d4f8ef394a61c264
jose-wellington554 <wellinton554081@gmail.com>
2025-08-10 13:45:25 -0300

    adicionando mais moedas

--- scripts.js
+++ scripts.js
@@ -9,6 +9,10 @@ function convertValues() {
     const currencyValueConverted = document.querySelector(".curreny-value") //valor convertido
     const dolartody = 5.2
     const eurotody = 6.2
+    const libratody =7.30
+    const bitcointody = 633799.00
+    const real =1.0
+
     if (conrrencytSelect.value == "dolar")
         //SE O SELECT  SELECIONADO FOR "DOLAR", ENTRE AQUI 
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
@@ -23,10 +27,30 @@ function convertValues() {
             currency: "EUR"
         }).format(inputCurrecyValue / eurotody)
 
-     currencyValueToCovert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    if (conrrencytSelect.value == "Libra")
+        //SE O SELECT  SELECIONADO FOR "UERO", ENTRE AQUI 
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputCurrecyValue / libratody)
+
+    if (conrrencytSelect.value == "bitcoin")
+        //SE O SELECT  SELECIONADO FOR "UERO", ENTRE AQUI 
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "BTC"
+        }).format(inputCurrecyValue / bitcointody)  
+
+
+
+
+    if (currencyValueToCovert. value== "real")
+    currencyValueToCovert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
     }).format(inputCurrecyValue)
+
+
 }
 
 function changeCurrency() {
@@ -41,6 +65,16 @@ function changeCurrency() {
     if (conrrencytSelect.value == "euro"){
         currencyName.innerHTML = "Euro" 
         currecyImagem.src = "./assets/EURO.png"
+    }
+
+    if (conrrencytSelect.value == "Libra"){
+        currencyName.innerHTML = "Libra" 
+        currecyImagem.src = "assets/libra.png"
+    }
+
+     if (conrrencytSelect.value == "bitcoin"){
+        currencyName.innerHTML = "bitcoin" 
+        currecyImagem.src ="./assets/BITCOIN.png"
     }
 
     convertValues()
